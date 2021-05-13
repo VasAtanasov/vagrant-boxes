c996474aec448ca297d493c3417768e75ef67b15
Vasil Atanasov <vas.atanasov@gmail.com>
2021-05-13 22:11:04 +0300

    refactored java and maven install script

--- scripts/sdkman-java-mvn.sh
+++ scripts/sdkman-java-mvn.sh
@@ -1,23 +1,38 @@
 
-VERSION="11.0.6-amzn"
-MAVEN_VERSION="3.6.3"
+JAVA_VERSION=${JAVA_VERSION:-"11.0.11.9.1-amzn"}
+MAVEN_VERSION=${MAVEN_VERSION:-"3.6.3"}
+
+function execAsUser() {
+    local username=${1}
+    local exec_command=${2}
+    sudo -u "${username}" -H bash -c "${exec_command}"
+}
 
 apt-get update && \
 	apt-get install -y zip unzip curl && \
 	rm -rf /var/lib/apt/lists/* && \
 	rm -rf /tmp/*
 
-curl -s "https://get.sdkman.io" | bash
+USERNAME=${USERNAME:-vagrant}
+
+execAsUser "${USERNAME}" "curl -s "https://get.sdkman.io" | bash"
 
 # Installing Java and Maven, removing some unnecessary SDKMAN files
-bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && \
+execAsUser "${USERNAME}" "source /home/${USERNAME}/.sdkman/bin/sdkman-init.sh && \
     yes | sdk install java $JAVA_VERSION && \
     yes | sdk install maven $MAVEN_VERSION && \
-    rm -rf $HOME/.sdkman/archives/* && \
-    rm -rf $HOME/.sdkman/tmp/*"
+    rm -rf /home/$USERNAME/.sdkman/archives/* && \
+    rm -rf /home/$USERNAME/.sdkman/tmp/*"
 
-# ENTRYPOINT bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && $0 $@" 
+INSTALL_DIR="/home/${USERNAME}/.sdkman/candidates"
+MAVEN_INSTALL_DIR="${INSTALL_DIR}/maven/current"
 
-MAVEN_HOME="$HOME/.sdkman/candidates/maven/current" 
-JAVA_HOME="$HOME/.sdkman/candidates/java/current" 
-PATH="$MAVEN_HOME/bin:$JAVA_HOME/bin:$PATH" 
+cat <<EOF >/etc/profile.d/maven.sh
+#!/bin/sh
+export MAVEN_HOME=${MAVEN_INSTALL_DIR}
+export M2_HOME=${MAVEN_INSTALL_DIR}
+export M2=${MAVEN_INSTALL_DIR}/bin
+export PATH=${MAVEN_INSTALL_DIR}/bin:$PATH
+EOF
+
+source /etc/profile.d/maven.sh
