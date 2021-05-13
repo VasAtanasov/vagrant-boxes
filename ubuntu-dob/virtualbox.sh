9574e54392c59e387f6dc8737018556e8d90b224
Vasil Atanasov <vas.atanasov@gmail.com>
2021-05-13 09:34:17 +0300

    installing virtualbox guest additions script

--- ubuntu-dob/virtualbox.sh
+++ ubuntu-dob/virtualbox.sh
@@ -3,36 +3,55 @@
 # set a default HOME_DIR environment variable if not set
 HOME_DIR="${HOME_DIR:-/home/vagrant}"
 
-case "$PACKER_BUILDER_TYPE" in
-virtualbox-iso | virtualbox-ovf)
-  VER="$(cat "$HOME_DIR"/.vbox_version)"
-  ISO="VBoxGuestAdditions_$VER.iso"
+VBOX_VER=$(dmidecode | grep -i vboxver | grep -E -o '[[:digit:]\.]+' | tail -n 1)
+ISO="VBoxGuestAdditions_$VBOX_VER.iso"
+WEBSERVER="https://download.virtualbox.org/virtualbox/${VBOX_VER}"
+URL="${WEBSERVER}/${ISO}"
+LOCAL_FILE="${HOME_DIR}/${ISO}"
 
-  # mount the ISO to /tmp/vbox
-  mkdir -p /tmp/vbox
-  mount -o loop $HOME_DIR/$ISO /tmp/vbox
+if command -v wget >/dev/null 2>&1; then
+  wget --no-check-certificate "$URL" -P "${HOME_DIR}/"
+elif command -v curl >/dev/null 2>&1; then
+  curl --insecure --location "$URL" --output "${LOCAL_FILE}"
+else
+  echo "Cannot download $ISO"
+  exit 1
+fi
 
-  echo "installing deps necessary to compile kernel modules"
-  apt-get install -y build-essential dkms bzip2 tar linux-headers-$(uname -r)
+# mount the ISO to /tmp/vbox
+mkdir -p /tmp/vbox
+mount -o loop "$LOCAL_FILE" /tmp/vbox
 
-  echo "installing the vbox additions"
-  # this install script fails with non-zero exit codes for no apparent reason so we need better ways to know if it worked
-  /tmp/vbox/VBoxLinuxAdditions.run --nox11 || true
+echo "installing deps necessary to compile kernel modules"
+apt-get install -y build-essential dkms bzip2 tar linux-headers-"$(uname -r)"
 
-  if ! modinfo vboxsf >/dev/null 2>&1; then
-    echo "Cannot find vbox kernel module. Installation of guest additions unsuccessful!"
-    exit 1
-  fi
+echo "installing the vbox additions"
+# this install script fails with non-zero exit codes for no apparent reason so we need better ways to know if it worked
+/tmp/vbox/VBoxLinuxAdditions.run --nox11 || true
 
-  echo "unmounting and removing the vbox ISO"
-  umount /tmp/vbox
-  rm -rf /tmp/vbox
-  rm -f "$HOME_DIR"/*.iso
+if ! modinfo vboxsf >/dev/null 2>&1; then
+  echo "Cannot find vbox kernel module. Installation of guest additions unsuccessful!"
+  exit 1
+fi
 
-  echo "removing kernel dev packages and compilers we no longer need"
-  apt-get remove -y build-essential gcc g++ make libc6-dev dkms linux-headers-$(uname -r)
+echo "unmounting and removing the vbox ISO"
+umount /tmp/vbox
+rm -rf /tmp/vbox
+rm -f "$LOCAL_FILE"
 
-  echo "removing leftover logs"
-  rm -rf /var/log/vboxadd*
-  ;;
-esac
+echo "removing kernel dev packages and compilers we no longer need"
+apt-get remove -y build-essential gcc g++ make libc6-dev dkms linux-headers-"$(uname -r)"
+
+echo "removing leftover logs"
+rm -rf /var/log/vboxadd*
+
+username="vagrant"
+vbGroup="vboxsf"
+
+echo "===> Adding ${username} user to the ${vbGroup} group"
+
+if grep -q $vbGroup /etc/group; then
+  sudo usermod -aG ${vbGroup} ${username}
+else
+  echo "Group ${vbGroup} does not exist"
+fi
